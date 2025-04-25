585dbc7179754cab111f6a872c02f85c471b22ad
dezmerek <mszymanski027@gmail.com>
2025-04-25 11:46:17 +0200

    feat: Add Navbar component to the layout

---
+++ src/components/main/navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <nav className="bg-white">
+      <div className="text-2xl font-bold bg-gradient-to-r from-emerald-500 bg-clip-text text-transparent">
+        AthletiX
+      </div>
+    </nav>
+  );
+}
